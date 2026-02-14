cccefa4ab20f7760c32e40d97c18ebc672dfd740
toperic100 <mr.eric.tan@gmail.com>
2026-02-14 12:24:56 +0800

    Update template.module.ts

--- backend/template-service/src/template.module.ts
+++ backend/template-service/src/template.module.ts
@@ -1,1 +1,12 @@
 
+// backend/template-service/src/template.module.ts
+import { Module } from '@nestjs/common';
+import { TemplateController } from './template.controller';
+import { TemplateManagerService } from './template-manager.service';
+
+@Module({
+  controllers: [TemplateController],
+  providers: [TemplateManagerService],
+  exports: [TemplateManagerService],
+})
+export class TemplateModule {}
