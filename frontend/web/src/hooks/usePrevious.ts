deec317303f88f9309d6ccf8937324e1e29e988c
toperic100 <mr.eric.tan@gmail.com>
2026-02-14 10:36:49 +0800

    Update usePrevious.ts

--- frontend/web/src/hooks/usePrevious.ts
+++ frontend/web/src/hooks/usePrevious.ts
@@ -1,1 +1,16 @@
 
+// frontend/web/src/hooks/usePrevious.ts
+import { useEffect, useRef } from 'react';
+
+/**
+ * 获取前一个值Hook
+ */
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
